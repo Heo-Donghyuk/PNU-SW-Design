1167ea8c5750a5a18889b192d3a2df9db1564017
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-20 17:10:12 +0900

    refactor: 화면 전환을 Intent를 이용한 Activity 전환으로 변경

--- app/src/main/java/com/pnu/myweather/MainActivity.kt
+++ app/src/main/java/com/pnu/myweather/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.pnu.myweather
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.navigation.compose.rememberNavController
-import com.pnu.myweather.core.navigation.AppNavGraph
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import com.pnu.myweather.feature.main.view.HomeScreenActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val navController = rememberNavController()
-            Surface(color = MaterialTheme.colorScheme.background) {
-                AppNavGraph(navController = navController)
-            }
-        }
+        val intent = Intent(this, HomeScreenActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
