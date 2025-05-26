b774fd457c3d401d817060e482ebac73af6a6c0e
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-26 22:36:30 +0900

    feat: 날씨 API 클라이언트 정의

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.mediapipe:tasks-genai:0.10.22")
 
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
     // Default
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
---
+++ app/src/main/java/com/pnu/myweather/core/weather/WeatherApiClient.kt
@@ -0,0 +1,19 @@
+package com.pnu.myweather.core.weather
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object WeatherApiClient {
+    private const val BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val service: WeatherApiService by lazy {
+        retrofit.create(WeatherApiService::class.java)
+    }
+}
